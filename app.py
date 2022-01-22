bae85e771d286734def44476b94c7f908d4e8548
JoaoNogaroli <70725289+JoaoNogaroli@users.noreply.github.com>
2022-01-22 15:34:14 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import cv2
 app =Flask(__name__)
 
 global camera
-camera = cv2.VideoCapture(-1)
+camera = cv2.VideoCapture(0)
 
 def generate_frames():
     while True:
@@ -31,4 +31,4 @@ def video():
     return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run()
